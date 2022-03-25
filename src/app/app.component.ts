fe38484dfda1318d23272cedccbdc699da0c4696
dhartl <dominikhartl24@gmail.com>
2022-03-25 22:54:06 +0100

    next add counter , deploy on k8s and test everything

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,67 @@
+import { animate, style, transition, trigger } from '@angular/animations';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { interval } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger(
+      'inOutAnimation', 
+      [
+        transition(
+          ':enter', 
+          [
+            transition(':enter', [
+              animate(600, style({opacity: 1}))
+            ]),
+          ]
+        ),
+        transition(
+          ':leave', 
+          [
+            animate(600, style({opacity: 0}))
+          ]
+        )
+      ]
+    )
+  ]
 })
 export class AppComponent {
-  title = 'k3s-dashboard';
-  shouldIncrease = false;
-  increase(){
-      this.shouldIncrease = true;
-      let counter = 0;
-      while(this.shouldIncrease) {
-        counter = counter * counter + 2500;
-      }
+  pods = [];
+  clicked = false;
+  decreasing = false;
+  constructor(private _snackBar: MatSnackBar, private http: HttpClient) {
+    this.getPods();
   }
 
-  stop(){
-    this.shouldIncrease = false;
+  createLoad() {
+    this.clicked = true;
+    for (let i = 0; i < 50; i++) {
+      this.http.get('http://localhost:30081/test').subscribe(result => {
+        if (i == 49) {
+          this.clicked = false;
+        }
+      }, error => {
+        if (i == 49) {
+          this.clicked = false;
+        }
+      });
+    }
+    this._snackBar.open("Auslastung gestartet", "OK");
   }
+
+  getPods() {
+    interval(2000).subscribe(x => {
+      this.http.get<any>('http://localhost:30081/pods').subscribe(pods => {
+        console.log(pods);
+        this.pods = pods.items;
+      });
+    });
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,9 +13,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSnackBarModule,
+    HttpClientModule,
+
   ],
-  providers: [],
+  providers: [HttpClientModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
