cd7c0d53ddcf9540e45aeabbb23bf448a3376123
dhartl <dominikhartl24@gmail.com>
2022-03-26 21:41:45 +0100

    fixed test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,6 @@ describe('AppComponent', () => {
   it(`should have as title 'k3s-dashboard'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('k3s-dashboard');
   });
 
   it('should render title', () => {
